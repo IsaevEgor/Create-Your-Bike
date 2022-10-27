be0a06c6c6b8ba434e1fea2a5d3dd216804d62dd
IsaevEgor <isaevegor.mail@gmail.com>
2022-10-27 14:47:45 +0300

    add build files

--- src/component/card.tsx
+++ src/component/card.tsx
@@ -1,21 +1,48 @@
 import React, {useState} from 'react';
 import { CSSTransition, SwitchTransition, Transition } from 'react-transition-group';
+import { start } from 'repl';
 import { text } from 'stream/consumers';
 import Button from './atoms/Button';
 import Cover from './atoms/cover';
 import cl from "./card.module.css";
 import moveBlock from "./cssTransitions/moveBlock.module.css";
 import moveButton from "./cssTransitions/moveButton.module.css";
+import moveVariantBlock from "./cssTransitions/moveVariantBlock.module.css";
+import moveText from "./cssTransitions/moveText.module.css";
 import Bike from './molecules/Bike';
   
 const Card = () => {
-	const [goLeft, setGoLeft] = useState(false)
+	const [goAnimation, setGoAnimation] = useState(false)
+	const [replaceWheels, setReplaceWheels] = useState(false)
+	const [typeWheels, setTypeWheels] = useState('')
+
+	function returnTrueWheels(type:string) {
+		setTypeWheels(type)
+	}
 
 	return (
 		<div>
 			<Cover/>
-			<Bike/>
-			<CSSTransition in={goLeft} timeout={5000000} classNames={moveBlock}>
+			<Bike 
+				goAnimation={goAnimation}
+				replaceWheels={replaceWheels}
+				typeWheels={typeWheels}
+			/>
+			{/*
+			<SwitchTransition mode='out-in'>
+				<CSSTransition
+					key={goAnimation}
+					classNames={moveText}
+					timeout={1000}
+				>
+					{goAnimation 
+					 ? <p className={cl.SecondText}>Отлично выглядит!</p>
+					 : <p className={cl.StartText}>Создай Свой Bike!</p>
+					}
+				</CSSTransition>
+			</SwitchTransition>
+			*/}
+			<CSSTransition in={goAnimation} timeout={1500} classNames={moveBlock}>
 			<div 
 						className={`${cl.ContentBlock}`}
 						id="contentBlock"
@@ -23,22 +50,22 @@ const Card = () => {
 
 							<SwitchTransition mode='out-in'>
 								<CSSTransition
-									key={goLeft}
+									key={goAnimation}
 									timeout={1000}
 									classNames={moveButton}
 									>
 									<div className={cl.BlockButton}>
 										<Button 
 											className={cl.Button}
-											onClick={() =>setGoLeft(!goLeft)}
-											style={goLeft ? {backgroundColor: "transparent", color: "#ff4800"} : {color: "white"}}
+											onClick={() =>setGoAnimation(!goAnimation)}
+											style={goAnimation ? {backgroundColor: "transparent", color: "#ff4800"} : {color: "white"}}
 										>
-											{goLeft ? "Назад" : "далее"}
+											{goAnimation ? "Назад" : "далее"}
 										</Button>
-										{goLeft 
+										{goAnimation 
 											?
 											<Button 
-												className={cl.ButtonBuy} 
+												className={cl.ButtonBuy}
 											>
 												Купить
 											</Button>
@@ -50,7 +77,52 @@ const Card = () => {
 
 					</div>
 			</CSSTransition>
-
+			<div className={cl.BlockHidden}>
+				<CSSTransition
+					in={goAnimation}
+					timeout={2000}
+					classNames={moveVariantBlock}
+				>
+					<div className={cl.Block}>
+						<h4 className={cl.Title}>Колеса</h4>
+						<span className={cl.Span}>(для смены сделайте двойной клик)</span>
+						<div className={cl.FlexBlock}>
+							<div className={cl.Item}
+								onClick={() => {
+									returnTrueWheels('type1')
+									setReplaceWheels(!replaceWheels)
+								}}
+							>
+								<div className={cl.Img}/>
+							</div>
+							<div className={cl.Item}
+								onClick={() => {
+									returnTrueWheels('type2')
+									setReplaceWheels(!replaceWheels)
+								}}
+							>
+								<div className={cl.Img}/>
+							</div>
+							<div className={cl.Item}
+								onClick={() => {
+									returnTrueWheels('type3')
+									setReplaceWheels(!replaceWheels)
+								}}
+							>
+								<div className={cl.Img}/>
+							</div>
+							<div className={cl.Item}
+								onClick={() => {
+									returnTrueWheels('type4')
+									setReplaceWheels(!replaceWheels)
+								}}
+							>
+								<div className={cl.Img}/>
+							</div>
+						</div>
+					</div>
+				</CSSTransition>
+			</div>
 		</div>
 	);
 };
--- src/component/molecules/Bike.tsx
+++ src/component/molecules/Bike.tsx
@@ -1,11 +1,70 @@
 import React from 'react';
+import { CSSTransition } from 'react-transition-group';
 import cl from "./bike.module.css";
+import moveBike from "../cssTransitions/moveBike.module.css";
+import moveWheelBack from "../cssTransitions/moveWheelBack.module.css";
+import moveWheelFront from "../cssTransitions/moveWheelFront.module.css";
+import ramaUp from "../cssTransitions/rama-up.module.css";
+import "../cssTransitions/replaceWheel.css";
+
+interface IBike {
+	goAnimation: boolean
+	replaceWheels: boolean
+	typeWheels: string
+}
   
-const Bike = () => {
+const Bike = ({goAnimation, replaceWheels, typeWheels}:IBike) => {
+	console.log(replaceWheels);
+	
+	
 	return (
 		<div className={cl.BikeBlock}>
-			<div className={cl.Rama}></div>
-			<div className={cl.Sedlo}></div>
+			<CSSTransition
+				timeout={2000} 
+				in={goAnimation} 
+				classNames={moveBike}
+			>
+				<CSSTransition 
+					timeout={3000}
+					in={replaceWheels}
+					classNames={ramaUp}
+					exit={false}
+				>
+					<div className={cl.Rama}>
+						<div className={cl.Sedlo}></div>
+						<CSSTransition
+							timeout={2000} 
+							in={goAnimation} 
+							classNames={moveWheelBack}
+						>
+							<CSSTransition 
+								timeout={1800}
+								in={replaceWheels}
+								classNames={`back-${typeWheels}`}
+								exit={false}
+							>
+								<div className={cl.WheelBack}></div>
+							</CSSTransition>
+						</CSSTransition>
+						<CSSTransition
+							timeout={2000} 
+							in={goAnimation} 
+							classNames={moveWheelFront}
+						>	
+							<CSSTransition 
+								timeout={2500}
+								in={replaceWheels}
+								classNames={`front-${typeWheels}`}
+								exit={false}
+							>
+								<div className={cl.WheelFront}></div>
+							</CSSTransition>
+
+						</CSSTransition>
+					</div>
+				</CSSTransition>
+
+			</CSSTransition>
 		</div>
 	);
 };
